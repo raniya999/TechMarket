0ac31372be950b7ac35a2b91646ce464144fbc3b
Raniya <alikulraniya0@gmail.com>
2026-07-08 17:56:47 +0500

    Fix: Save and upload completed serializers

---
+++ shop/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import Brand, Category, Phone, Order
+
+class BrandSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=100)
+    country = serializers.CharField(max_length=100, required=False, allow_blank=True)
+
+    def create(self, validated_data):
+        return Brand.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.country = validated_data.get('country', instance.country)
+        instance.save()
+        return instance
+
+
+class CategorySerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=100)
+    slug = serializers.SlugField()
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.slug = validated_data.get('slug', instance.slug)
+        instance.save()
+        return instance
+
+
+class PhoneSerializer(serializers.ModelSerializer):
+    brand_name = serializers.CharField(source='brand.name', read_only=True)
+    category_name = serializers.CharField(source='category.name', read_only=True)
+
+    class Meta:
+        model = Phone
+        fields = ['id', 'brand', 'brand_name', 'category', 'category_name', 'model_name', 'price', 'description']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'  
