1a2f742b93b0c2f6b43fafb0a90f79c93e869f4d
Raniya <alikulraniya0@gmail.com>
2026-07-08 22:04:00 +0500

    Fix project bugs and update views

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,17 +1,36 @@
 from rest_framework import serializers
 from .models import Brand, Category, Phone, Order
 
-class BrandSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Brand
-        fields = ['id', 'name', 'country']
+class BrandSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=100)
+    country = serializers.CharField(max_length=100, allow_blank=True, required=False)
+
+    def create(self, validated_data):
+        return Brand.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.country = validated_data.get('country', instance.country)
+        instance.save()
+        return instance
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name', 'description', 'icon']
+class CategorySerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=100)
+    description = serializers.CharField(allow_blank=True, required=False)
+    icon = serializers.CharField(max_length=40, allow_blank=True, required=False)
 
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.icon = validated_data.get('icon', instance.icon)
+        instance.save()
+        return instance
 
 class PhoneSerializer(serializers.ModelSerializer):
     brand_name = serializers.CharField(source='brand.name', read_only=True)
